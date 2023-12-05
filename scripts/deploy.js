01ce4861ea56f07d04794efd827d08c550a14f80
Oskar Svanström <oskar.svanstrom@hotmail.com>
2023-12-05 16:43:00 +0100

    userData tests

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,22 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const blockThoughts = await hre.ethers.deployContract("BlockThoughts");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  await blockThoughts.waitForDeployment();
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`BlockThoughts deployed`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
---
+++ test/userData.test.js
@@ -0,0 +1,55 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('BlockThoughs Contract Userdata tests', function () {
+  let BlockThoughts;
+  let blockThoughts;
+  let owner;
+  let addr1;
+  let addr2;
+
+  // Before each test, deploy the contract
+  beforeEach(async function () {
+    BlockThoughts = await ethers.getContractFactory('BlockThoughts');
+    [owner, addr1, addr2] = await ethers.getSigners();
+    
+    blockThoughts = await BlockThoughts.deploy();
+  });
+
+  // Tests createUserData and getUserdata
+  it('Should create user data', async function () {
+    const username = 'oskar felixsson';
+    const ipfsImageRef = ethers.encodeBytes32String('ipfsHash');
+    
+    await blockThoughts.createUserData(username, ipfsImageRef);
+
+    const userData = await blockThoughts.getUserData(owner.address);
+
+    expect(userData.username).to.equal(username);
+    expect(userData.ipfsImageRef).to.equal(ipfsImageRef);
+  });
+
+  it('Should not allow empty username', async function () {
+    const username = '';
+    const ipfsImageRef = ethers.encodeBytes32String('ipfsHash');
+    
+    await expect(blockThoughts.createUserData(username, ipfsImageRef)).to.be.revertedWith(
+        'Username cannot be empty'
+      );
+  });
+
+  // Tests _canCreateUserData
+  it('Should not allow creation of duplicate user data', async function () {
+    const username = 'oskar felixsson';
+    const ipfsImageRef = ethers.encodeBytes32String('ipfsHash');
+
+    await blockThoughts.createUserData(username, ipfsImageRef);
+
+    const duplicateUsername = 'oskar felixsson';
+    const duplicateIpfsImageRef = ethers.encodeBytes32String('ipfsHash2');
+
+    await expect(blockThoughts.createUserData(duplicateUsername, duplicateIpfsImageRef)).to.be.revertedWith(
+      'Username or address already exists'
+    );
+  });
+});
