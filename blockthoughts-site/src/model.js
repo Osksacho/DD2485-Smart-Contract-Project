38137419f5e276b4680eac3c5377fbff9fda0214
felixq <felixq@kth.se>
2023-12-12 17:17:16 +0100

    Started on connection to contract

--- blockthoughts-site/src/model.js
+++ blockthoughts-site/src/model.js
@@ -1,9 +1,20 @@
 
 class Thread {
-  constructor(id, subject) {
+  constructor(id, subject, op) {
     this.id = id;
     this.subject = subject;
+    this.op = op;
+    this.comments = [];
   }
+}
+
+class Comment {
+  constructor (text, address) {
+    this.comment = text;
+    this.address = address;
+    this.alias = get_alias();
+  }
+
 }
 
 class Model {
@@ -12,6 +23,8 @@ class Model {
     this.currentThreadId = -1;
     this.currentThreadComments = [];
 
+    this.contractABI = ""/* Your Contract ABI */;
+    this.contractAddress = ""/* Your Contract Address */;
     // Just for tests
     const test = ["This is a thread"
     , "I am a cool dude"
@@ -26,20 +39,49 @@ class Model {
 
     this.threads = test.map(strToThread);
     // Just for tests
-   }
+  }
 
+  async getThreads() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []); // Request access to MetaMask
+    const signer = provider.getSigner();
 
-   getThreads() {
+    const contract = new ethers.Contract(this.contractAddress, this.contractABI, signer);
+
+    try {
+      const threadsData = await contract.getThreads();
+      this.threads = threadsData.map(thread => new Thread(thread.id, thread.subject, thread.poster));
+    } catch (error) {
+      console.error("Error fetching threads:", error);
+    }
+  }
+
+  get_alias(address) {
+    return "";
+  }
+  
+  getThreads() {
     return [];
-   }
+  }
 
-   getComment(ipfsHash) {
-
-   }
-
-   getComments(threadId) {
+  getComment(ipfsHash, address) {
     
-   }
+    // GET Text from ipfs
+    const text = "";
+
+
+
+    return new Comment(); 
+  }
+
+  getComments(threadId) {
+    // GET array of comments from contract
+    const contractComments = [];
+
+    const comments = contractComments.map(this.getComment);
+
+
+  }
 }
 
 export default Model;
